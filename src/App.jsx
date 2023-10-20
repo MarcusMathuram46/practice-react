265dd9e3533196fcfdc6fec4c5420a22b5606369
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2023-10-20 11:43:54 +0530

    1st of the dynamic compoonent

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from 'react'
 
 function App() {
-
-  
   const now = new Date ();
-
-  // simple addition
-  const a = 25;
-  const b = 25;
+  console.log(now);
 
   return (
     <div>
-      <p>Hello World, it is {now.toString()}</p>
-
-      <p>
-        {a} plus {b} is 50
-      </p>
+      <p>Hello World</p>
+      
     </div>
   )
 }
 
 export default App
-
-
