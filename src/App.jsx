76d660d528d7eefcfb78affe1263ad903cee74f5
MarcusMathuram46 <marcus.mathuram7@gmail.com>
2023-10-20 11:26:10 +0530

    addingdynamic content to a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-function  () {
+function App() {
+
+  
+  const now = new Date ();
+
+  // simple addition
+  const a = 25;
+  const b = 25;
+
   return (
     <div>
-      
+      <p>Hello World, it is {now.toString()}</p>
+
+      <p>
+        {a} plus {b} is 50
+      </p>
     </div>
   )
 }
 
-export default  
+export default App
+
+
